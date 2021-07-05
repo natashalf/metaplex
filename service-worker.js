d85daaa40366942c19027fe58756dfa51bcae008
Natasha Fawn <natasha@Natashas-iMac.local>
2021-07-05 20:26:29 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.4f4f07b2786f411c6e4864efe001bdc8.js"
+  "./precache-manifest.71b96d401ed0d44ebc67269881322e7b.js"
 );
 
 self.addEventListener('message', (event) => {
